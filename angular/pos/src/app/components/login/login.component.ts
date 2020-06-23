923c105d5e0308527a0d7d26ff301d05b2361df5
roberto-silva <roberto.silva.oli@outlook.com>
2020-06-23 14:22:56 -0300

    initial - Refactoring method name (Translated to English)

--- angular/pos/src/app/components/login/login.component.ts
+++ angular/pos/src/app/components/login/login.component.ts
@@ -9,34 +9,34 @@ import { FormBuilder, FormGroup, Validators, FormControl  } from '@angular/forms
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  formularioLogin: FormGroup;
+  formLogin: FormGroup;
   constructor(private loginService: LoginService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.criarFormularioLogin();
+    this.createForm();
     if (localStorage.getItem('token') !== null &&
     localStorage.getItem('token').toString().trim() !== null) {
       this.loginService.goToHome();
     }
   }
-  criarFormularioLogin() {
-    this.formularioLogin = this.fb.group({
+  createForm() {
+    this.formLogin = this.fb.group({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required])
     });
   }
   login() {
     localStorage.clear();
-    this.loginService.login(this.formularioLogin.value);
+    this.loginService.login(this.formLogin.value);
   }
   getValidation = (controlName: string, errorName: string) =>{
     if(this.inputTouched(controlName)){
-      return this.formularioLogin.controls[controlName].hasError(errorName);
+      return this.formLogin.controls[controlName].hasError(errorName);
     }
     return false;
   }
   inputTouched(field){
-    return this.formularioLogin.get(field).touched;
+    return this.formLogin.get(field).touched;
   }
 
 }

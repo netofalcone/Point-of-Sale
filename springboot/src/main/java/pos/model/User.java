ef18fc03e48565d9eb0a693b693b4ebc37bf7fcf
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2020-07-03 17:09:42 -0300

    migration insert-permission - refatoção das classes e imports

--- springboot/src/main/java/pos/model/User.java
+++ springboot/src/main/java/pos/model/User.java
@@ -2,12 +2,22 @@ package pos.model;
 
 import java.io.Serializable;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
-@Entity(name = "User")
+@Entity(name = "user")
 public class User implements Serializable {
 
-    @Id
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
--- springboot/src/main/resources/db/migration/V1.2__Description.sql
+++ springboot/src/main/resources/db/migration/V1.2__Description.sql
@@ -1,18 +1,26 @@
-CREATE TABLE permission(
+CREATE TABLE permission (
 	id INTEGER NOT NULL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	description VARCHAR(250) NULL,
 	deleted BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE role(
+CREATE TABLE role (
 	id INTEGER NOT NULL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	description VARCHAR(250) NULL,
 	deleted BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE User(
+CREATE TABLE role_permission (
+	id_role INTEGER NOT NULL,
+	id_permission INTEGER NOT NULL,
+	PRIMARY KEY (id_role, id_permission),
+	CONSTRAINT role_permission_role_FK FOREIGN KEY (id_role) REFERENCES role (id),
+	CONSTRAINT role_permission_permission_FK FOREIGN KEY (id_permission) REFERENCES permission (id)
+);
+
+CREATE TABLE public.user (
 	id INTEGER NOT NULL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	email VARCHAR(250) NOT null,
@@ -21,5 +29,5 @@ CREATE TABLE User(
 	cpf VARCHAR(50) NOT NULL,
 	deleted BOOLEAN NOT null DEFAULT FALSE,
 	idrole INTEGER NOT null,
-	CONSTRAINT user_role_FK FOREIGN KEY (idrole) REFERENCES "role" (id)
+	CONSTRAINT user_role_FK FOREIGN KEY (idrole) REFERENCES role (id)
 );
